6037fec4b0698af147365ce855f515f81e184b3c
justin-moretto <JustinMorettoMTL@gmail.com>
2020-11-05 03:59:20 +0000

    created input.js and moved input code over there

---
+++ input.js
@@ -0,0 +1,18 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => handleUserInput(key));
+  return stdin;
+};
+//setupInput();
+
+const handleUserInput = function(key) {
+  // \u0003 maps to ctrl+c input which exits the process
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-// play.js
-const connect = require('./client.js');
+const connect = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => handleUserInput(key));
-  return stdin;
-}
 setupInput();
-
-const handleUserInput = function(key) {
-  // \u0003 maps to ctrl+c input which exits the process
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
