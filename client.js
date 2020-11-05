5fe144aa1349c1fe40ae858a784748d70894a4bd
justin-moretto <JustinMorettoMTL@gmail.com>
2020-11-05 03:17:42 +0000

    added client side connection message; connecting now sends snake name to server

--- client.js
+++ client.js
@@ -1,14 +1,16 @@
 const net = require('net');
-var events = require('events');
+const events = require('events');
 
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542
   });
+  conn.on('connect', () => console.log('succesfully connected to game server'));
   conn.on('data', data => console.log(data));
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
+  conn.write('Name: JMO');
   return conn;
-}
+};
 
 module.exports = connect;
