3d9793f8485d91bce9ce900f0b34d98b5b389473
justin-moretto <JustinMorettoMTL@gmail.com>
2020-11-05 04:30:47 +0000

    WASD movement implemented; three modules now communicate with each other to send strings to the server

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,9 +13,13 @@ const setupInput = function() {
 
 const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input which exits the process
+  console.log(key);
   if (key === '\u0003') {
     process.exit();
-  }
+  } else if (key === 'w') connection.write('Move: up');
+  else if (key === 'a') connection.write('Move: left');
+  else if (key === 's') connection.write('Move: down');
+  else if (key === 'd') connection.write('Move: right');
 };
 
 module.exports = {setupInput};
